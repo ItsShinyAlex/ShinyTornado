a308684e504a0b5174dd6955628a0e534f200c6f
Shiny Alex <carvantes.alex@gmail.com>
2018-08-01 02:06:11 -0500

    Start

---
+++ templates/home.php
@@ -0,0 +1,239 @@
+<?php
+    /* Template Name:  home TORNADO*/ 
+    get_header();
+?>
+
+<section id="tornado">
+    <div class="container fcentered">
+        <div class="flex fcentered">
+            <!--<div class="col small">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tornadoLOGO.png" alt="Logo de Tornado">
+            </div>-->
+            <div class="col mid fcentered">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tornado.png" alt="TORNADO">
+            </div>
+            <div class="col big">
+                <h2 class="white heavy" >SUEÑA·VIVE·VUELA</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="skydive">
+    <div class="container">
+        <div class="center fcentered">
+            <h2 class="shadow white">Skydive México</h2>
+            <p class="shadow white">
+                ¡LO TRAE PARA TI EN EL 
+                AERÓDROMO DE TEQUESQUITENGO!
+            </p>
+            <a href="" class="btn-navy">
+                ¿Cómo llegar?
+            </a>
+        </div>
+    </div>
+</section>
+
+<section id="tunel pad-right">
+    <div class="container">
+        <p class="blue titles">
+            TÚNEL
+        </p>
+        <div class="flex">
+            <div class="col nopad-left">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tunelSpace.png" alt="Túnel de Viento">
+            </div>
+            <div class="col center justify">
+                <p class="organge titles">
+                    DE VIENTO
+                </p>
+                <p>Tornado es un simulador de caída libre o túnel de viento que genera una corriente de aire continua que alcanza una velocidad de 270 km/h y  te permitirá experimentar la sensación de volar usando tu cuerpo.</p>
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/iconParachuter.png" alt="Caída libre">
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section id="howorks">
+    <div class="container">
+        <div class="flex">
+            <div class="col">
+                <h2 class="title-h2">
+                    <span class="left small-h2">
+                        ¿CÓMO
+                    </span><br>
+                    FUNCIONA?
+                    
+                </h2>
+                
+                <div class="centered">
+                    <p class="white">
+                        El túnel de viento ¡te hará VOLAR! con 6 ventiladores que crean un flujo de aire ascendente que te elevará. 
+                    </p>
+                    <p class="white">
+                        Un instructor experimentado te preparará con movimientos básicos para que puedas controlar el vuelo, así aprenderás a cómo cambiar la altura de tu vuelo con movimientos del cuerpo. Y entrará junto a ti, para estar al pendiente de que mantengas la posición correcta de tu cuerpo, así como brazos y piernas.
+                    </p>
+                    <p class="white">
+                        ¡Podrás volar sólo! no estarás atado al 
+                        instructor mientras él controla todos los movimientos.
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="whocan">
+    <div class="container flex">
+        <div class="col small"></div>
+        <div class="col">
+            <div class="centered">
+                <h2 class="title-h2 right">
+                    <span class="white right small-h2">
+                       ¿QUIÉN PUEDE
+                    </span><br>
+                    <span class="white right">
+                        VIVIR ESTA
+                    </span><br>
+                    <span class="orange">
+                        EXPERIENCIA?
+                    </span><br>
+                    
+                </h2>
+                
+                <p>
+                    Desde niños de 3 años hasta adultos mayores pueden vivir la experiencia de volar en esta atracción.
+                </p>
+                
+                <p><b>Sólo está restringida para:</b></p>
+                
+                <ul>
+                    <li>
+                        <p>Mujeres embarazadas.</p>
+                    </li>
+                    <li>
+                        <p>Personas que superen los 110kg.</p>
+                    </li>
+                    <li>
+                        <p>Personas que no estén en buen estado de salud.</p>
+                    </li>
+                    <li>
+                        <p>Personas con problemas en la columna vertebral o articulaciones (en particular los que sufren lesiones en los hombros).</p>
+                    </li>
+                    <li>
+                        <p>Personas con enfermedades cardiovasculares, del sistema nervioso, ansiedad o similar. </p>
+                    </li>
+                </ul>
+                
+                <p>Tampoco se puede ingresar al "tornado" bajo la influencia del alcohol u otras sustancias psicotrópicas.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="howmuch">
+    <div class="container">
+        <div class="left">
+            <h2 class="white">
+            
+                <span class="title-h2">¿CUÁNTO</span>
+                <span class="small-h2">CUESTA?</span>
+            
+            </h2>
+        </div>
+        
+        <div class="right">
+            <p class="white"><b>¡POR INAUGURACIÓN TENEMOS PRECIOS ESPECIALES!</b></p>
+        </div>
+        
+        <div class="center">
+            Precio Diver $190
+            <p class="mid">Precio Diver<span class="orange price">$190</span></p>
+            <p class="mid">Hora de Túnel<span class="orange price">$5,500</span> <br>(No transferible)</p>
+            <p class="mid"><b>¡NO TE LO PIERDAS!</b><br>
+                <span class="small">Para que no te quedes con las ganas,
+                también se ofrecen bloques de 2 minutos por</span><br>
+                <span class="orange price">$350</span>    
+                
+            </p>
+            
+            <div class="blue-border">
+                <p class="organge"><b>Vigencia 31 Agosto 2018</b></p>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section id="faq">
+    <div class="container">
+        <div class="bluebox">
+            <h2 class="title-h2 white">PREGUNTAS
+                <span class="orange right">FRECUENTES</span>
+            </h2>
+        </div>
+        
+        
+        
+        
+        <div class="center">
+            <p class="blue heavy">Y tú... ¿Te lo vas a perder?</p>
+        </div>
+    </div>
+</section>
+
+<section id="dream">
+    <div class="center">
+        <p class="heavy white">SI SOÑASTE CON VOLAR...</p>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tornado.png" alt="TORNADO">
+        <p class="heavy white">¡LO HACE REALIDAD!</p>
+        
+    </div>
+    <div class="orange-line center">
+        <a href="" class="btn-navy">
+                ¿Cómo llegar?
+        </a>
+    </div>
+</section>
+
+<section id="direccion">
+    <div class="container">
+        <div class="flex">
+            <div class="col">
+                <h2 class="blue">DIRECCIÓN AERÓDROMO DE TEQUESQUITENGO</h2>
+                <p>Emiliano Zapata #19 San José Vista Hermosa, Puente de Ixtla, Morelos. C.P. 62680</p>
+                <h2 class="orange">OFICINA EN CUERNAVACA
+                PARACAIDISMO MÉXICO AC</h2>
+                <p>Av de los 50 metros #100 Col. Villa Deportivas Torre III Oficina 406 C.P. 62389 Cuernavaca, Morelos.</p>
+                <h2 class="blue">HORARIOS DE ATENCIÓN</h2>
+                <p>Lunes a Domingo <br>
+                8:00 a 19:00 hrs</p>
+                <h2 class="orange">CORREO ELECTRÓNICO</h2>
+                <p>contacto@paracaidismo.mx</p>
+                <h2 class="blue">TELÉFONO Y WHATSAPP MÉXICO</h2>
+                <p>55 4044 0422  y  55 7397 0829</p>
+                <h2 class="orange">OFICINA EN CUERNAVACA</h2>
+                <p>01 777 171 5650</p>
+            </div>
+            <div class="col">
+                <div class="full-h">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="blue-line">
+            
+        </div>
+    </div>
+</section>
+
+
+
+<?php 
+    get_footer();
+?>
+
