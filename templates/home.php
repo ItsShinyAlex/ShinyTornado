9ee0dbf5b6a11be11509982a977cabf28531a5f2
Shiny Alex <carvantes.alex@gmail.com>
2018-08-02 23:24:49 -0500

    faq

--- templates/home.php
+++ templates/home.php
@@ -28,27 +28,30 @@
                 AERÓDROMO DE TEQUESQUITENGO!
             </p>
             <a href="" class="btn-navy">
-                ¿Cómo llegar?
+                <span class="rotated">¿Cómo llegar?</span>
             </a>
         </div>
     </div>
 </section>
 
-<section id="tunel pad-right">
+<section id="tunel" class="pad-right">
     <div class="container">
-        <p class="blue titles">
-            TÚNEL
+        <p class="blue titles giant">
+            TÚNEL 
         </p>
         <div class="flex">
-            <div class="col nopad-left">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tunelSpace.png" alt="Túnel de Viento">
+            <div class="col small">
+                <img id="wind-tunel" src="<?php echo get_stylesheet_directory_uri(); ?>/img/tunelSpace.png" alt="Túnel de Viento">
             </div>
-            <div class="col center justify">
-                <p class="organge titles">
+            <div class="col bigger justify">
+                <p class="orange titles big">
                     DE VIENTO
                 </p>
-                <p>Tornado es un simulador de caída libre o túnel de viento que genera una corriente de aire continua que alcanza una velocidad de 270 km/h y  te permitirá experimentar la sensación de volar usando tu cuerpo.</p>
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/iconParachuter.png" alt="Caída libre">
+                <p class="justified">Tornado es un simulador de caída libre o túnel de viento que genera una corriente de aire continua que alcanza una velocidad de 270 km/h y  te permitirá experimentar la sensación de volar usando tu cuerpo.</p>
+                <div class="fcentered">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/iconParachuter.png" alt="Caída libre">
+                </div>
+                
             </div>
             
         </div>
@@ -57,8 +60,8 @@
 
 <section id="howorks">
     <div class="container">
-        <div class="flex">
-            <div class="col">
+        <div class="flex pad-left">
+            <div class="col big">
                 <h2 class="title-h2">
                     <span class="left small-h2">
                         ¿CÓMO
@@ -67,7 +70,7 @@
                     
                 </h2>
                 
-                <div class="centered">
+                <div class="">
                     <p class="white">
                         El túnel de viento ¡te hará VOLAR! con 6 ventiladores que crean un flujo de aire ascendente que te elevará. 
                     </p>
@@ -80,7 +83,7 @@
                     </p>
                 </div>
             </div>
-            <div class="col">
+            <div class="col small">
                 
             </div>
         </div>
@@ -89,16 +92,23 @@
 
 <section id="whocan">
     <div class="container flex">
-        <div class="col small"></div>
-        <div class="col">
+        <div class="col small">
+            <img id="rnr" src="<?php echo get_stylesheet_directory_uri(); ?>/img/rockandrollguy.png" alt="Instructores de Tornado">
+        </div>
+        <div id="vivir" class="col">
             <div class="centered">
                 <h2 class="title-h2 right">
+                   
                     <span class="white right small-h2">
                        ¿QUIÉN PUEDE
                     </span><br>
                     <span class="white right">
                         VIVIR ESTA
-                    </span><br>
+                    </span>
+                    <span class="mov-change">
+                        <br>
+                    </span>
+                    
                     <span class="orange">
                         EXPERIENCIA?
                     </span><br>
@@ -140,8 +150,8 @@
         <div class="left">
             <h2 class="white">
             
-                <span class="title-h2">¿CUÁNTO</span>
-                <span class="small-h2">CUESTA?</span>
+                <span class="small-h2">¿CUÁNTO</span><br>
+                <span class="title-h2">CUESTA?</span>
             
             </h2>
         </div>
@@ -151,19 +161,24 @@
         </div>
         
         <div class="center">
-            Precio Diver $190
-            <p class="mid">Precio Diver<span class="orange price">$190</span></p>
-            <p class="mid">Hora de Túnel<span class="orange price">$5,500</span> <br>(No transferible)</p>
-            <p class="mid"><b>¡NO TE LO PIERDAS!</b><br>
-                <span class="small">Para que no te quedes con las ganas,
-                también se ofrecen bloques de 2 minutos por</span><br>
+            
+            <p class="mid pad-top">Precio Diver <span class="orange price">$190</span></p>
+            <p class="mid">Hora de Túnel <span class="orange price">$5,500</span> <br>(No transferible)</p>
+            <p class="mid no"><b>¡NO TE LO PIERDAS!</b><br>
+                <span class="small-p">Para que no te quedes con las ganas,
+                también se ofrecen bloques de 2 minutos por</span><br><br>
                 <span class="orange price">$350</span>    
                 
             </p>
-            
-            <div class="blue-border">
-                <p class="organge"><b>Vigencia 31 Agosto 2018</b></p>
+            <div class="fcentered">
+                
+                 <div class="blue-border fcentered">
+                    <p class="orange"><b>Vigencia 31 Agosto 2018</b></p>
+                </div>
+                
+                
             </div>
+           
             
         </div>
     </div>
@@ -171,16 +186,112 @@
 
 <section id="faq">
     <div class="container">
-        <div class="bluebox">
-            <h2 class="title-h2 white">PREGUNTAS
-                <span class="orange right">FRECUENTES</span>
-            </h2>
-        </div>
+       
+       <div class="fcentered">
+            <div class="bluebox">
+               <div class="pf">
+                   <h2  class="title-h2 white"><span id="preguntas">PREGUNTAS</span><img id="w-addon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/addon-preguntasF.png" alt=""> <br>
+                    <span id="frecuentes" class="orange right">FRECUENTES</span>
+                    </h2>
+               </div>
+               
+               <div class="fcentered">
+                   
+                   
+                               <div class="card">
+                      <article>
+                        <h4 class="effect" data-accordion-element-trigger>Elaboración de Contabilidad Electrónica.</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">La contabilidad electrónica se refiere a la obligación de llevar los registros y asientos contables a través de medios electrónicos e ingresar de forma mensual su información contable a través de la página de Internet del SAT.</p>
+                             <br>
+                           <p class="pad-bot"> Para dar cumplimiento formal al ingreso mensual de información contable, únicamente se enviará la balanza de comprobación y el catálogo de cuentas con el código agrupador del SAT que permita su interpretación.</p>
+
+
+                          
+                        </div>
+                      </article>
+                      
+                      <article>
+                        <h4 class="effect" data-accordion-element-trigger>Administración de la nómina.</h4>
+                        <div data-accordion-element-content class="content">
+                           <p class="pad-top">Consiste en contratar a una empresa para que efectúe un servicio, la contratación de personal, logrando con ésto beneficios tales como:</p>
+                            <h5>Generar Ahorro</h5>
+                            <p>*Ahorros Generales Tanto para el patrón como para los empleados</p>
+                            <h5>Percepción Extra</h5>
+                            <p>*Toda percepción extra está exenta tanto como para el patrón como para los empleados (comisiones, pago de bonos, etc).</p>
+                            <h5>Evita incrementar personal</h5>
+                            <p class="pad-bot" >*Evitando incrementar la plantilla del personal, debido a que no se contrata una persona sino a una empresa que provee servicios.</p>
+
+                        </div>
+                      </article>
+                      
+                      <article>
+                        <h4 class="effect" data-accordion-element-trigger>Elaboración de Estados Financieros.</h4>
+                        <div data-accordion-element-content class="content">
+                         
+                          <p class="pad-top">Los estados financieros son los documentos que debe preparar la empresa al terminar el ejercicio contable, con el fin de conocer la situación financiera y los resultados económicos obtenidos en sus actividades a lo largo del período</p>
+                          <br>
+                          <p>La información presentada en los estados financieros interesa a:</p>
+                          
+                          <ul>
+                              <li>La administración, para la toma de decisiones, después de conocer el rendimiento, crecimiento y desarrollo de la empresa durante un periodo determinado.</li>
+                              <li>Los propietarios para conocer el progreso financiero del negocio y la rentabilidad de sus aportes.</li>
+                              <li>Los acreedores, para conocer la liquidez de la empresa y la garantía de cumplimiento de sus obligaciones.</li>
+                              <li>El estado, para determinar si el pago de los impuestos y contribuciones esta correctamente liquidado.</li>
+                          </ul>
+                          
+                          <p>Los estados financieros básicos son:</p>
+                          
+                          <ul class="pad-bot">
+                              <li>Balance general.</li>
+                              <li>Estado de resultados.</li>
+                              <li>Estado de cambios en el patrimonio.</li>
+                              <li>Estado de cambios en la situación financiera (origen y aplicación de fondos).</li>
+                              <li>Estado de flujos de efectivo.</li>
+                          </ul>
+                        </div>
+                      </article>
+                      
+                      <article>
+                        <h4 class="effect" data-accordion-element-trigger>Determinación y presentación de pagos provisionales, pagos definitivos, declaraciones anuales, declaraciones informativas, etc.</h4>
+                        <div data-accordion-element-content class="content">
+                          <p class="pad-top">ISR, IVA, Retenciones, DIOTS, Riesgo de trabajo, etc.</p>
+                          <br>
+                          <p class="pad-bot">Todo de acuerdo a las obligaciones fiscales que tenga cada contribuyente registradas ante las autoridades fiscales y/o administrativas.</p>
+                        </div>
+                      </article>
+                      
+                       <article>
+                        <h4 class="effect" data-accordion-element-trigger>Determinación y presentación de Cuotas Obrero – Patronales ante el Instituto Mexicano del Seguro Social (IMSS). </h4>
+                        <div data-accordion-element-content class="content">
+                       
+                        </div>
+                      </article>
+                      
+                       <article>
+                        <h4 class="effect" data-accordion-element-trigger>Presentación de avisos mediante los diferentes portales de las autoridades fiscales y administrativas, así como la solventación de notificaciones.</h4>
+                        <div data-accordion-element-content class="content">
+                        
+                        </div>
+                      </article>
+                      
+                       <article>
+                        <h4 class="effect" data-accordion-element-trigger>Gestión ante las autoridades municipales para la obtención de la licencia de funcionamiento.</h4>
+                        <div data-accordion-element-content class="content">
+                       
+                        </div>
+                      </article>
+                    </div>
+               </div>
+                
+            </div>
+       </div>
+       
         
         
         
         
-        <div class="center">
+        <div class="fcentered">
             <p class="blue heavy">Y tú... ¿Te lo vas a perder?</p>
         </div>
     </div>
@@ -232,6 +343,179 @@
 </section>
 
 
+
+<script>
+    var gianniAccordion = (function(){
+      return class {      
+
+        transitionendEventName(){
+          var i,
+              el = document.createElement('div'),
+              transitions = {
+                'transition': 'transitionend',
+                'OTransition': 'otransitionend',
+                'MozTransition': 'transitionend',
+                'WebkitTransition': 'webkitTransitionEnd'
+              };
+
+          for (i in transitions) {
+            if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+              return transitions[i];
+            }
+          }
+        }
+
+        expand(el){
+          function resetHeight(ev){
+            if(ev.target != el.content) return
+            el.content.removeEventListener(this.transitionendevent, bindEvent);
+
+            if(!el.isOpen) return
+
+            requestAnimationFrame(()=>{      
+              el.content.style.transition = '0';
+              el.content.style.height = 'auto';
+
+              requestAnimationFrame(()=>{
+                el.content.style.height = null;
+                el.content.style.transition = null;
+
+                this.fire("elementOpened", el);
+              });
+            });
+          }
+
+          var bindEvent = resetHeight.bind(this);
+          el.content.addEventListener(this.transitionendevent, bindEvent);
+
+          el.isOpen = true;
+          el.content.style.height = el.content.scrollHeight + "px";
+        }
+
+        collapse(el){
+
+          function endTransition(ev){     
+            if(ev.target != el.content) return
+            el.content.removeEventListener(this.transitionendevent, bindEvent);
+
+            if(el.isOpen) return
+
+            this.fire("elementClosed", el);
+          }
+
+          var bindEvent = endTransition.bind(this);
+          el.content.addEventListener(this.transitionendevent, bindEvent);
+
+          el.isOpen = false;    
+
+          requestAnimationFrame(()=>{
+            el.content.style.transition = '0';
+            el.content.style.height = el.content.scrollHeight + "px";
+
+            requestAnimationFrame(()=>{
+              el.content.style.transition = null;
+              el.content.style.height = 0;
+            });
+          });
+        }
+
+        open(el){
+          el.selected = true;
+          this.fire("elementSelected", el);
+          this.expand(el);
+          el.wrapper.classList.add(this.selectedClass);
+        }
+
+        close(el){
+          el.selected = false;
+          this.fire("elementUnselected", el);
+          this.collapse(el);
+          el.wrapper.classList.remove(this.selectedClass);
+        }
+
+        toggle(el){
+          if(el.selected){
+            this.close(el);
+          } else {
+            this.open(el);
+
+            if(this.oneAtATime){        
+              this.els.filter(e=>e!=el && e.selected).forEach(e=>{
+                this.close(e);
+              });
+            }
+          }
+        }
+
+        attachEvents(){    
+          this.els.forEach((el, i)=>{
+            el.trigger.addEventListener("click", this.toggle.bind(this, el));
+          });
+        }
+
+        setDefaultData(){    
+          this.els = [];
+          this.events = {
+            'elementSelected': [],
+            'elementOpened': [],
+            'elementUnselected': [],
+            'elementClosed': []
+          };
+          this.transitionendevent = this.transitionendEventName();
+          this.oneAtATime = true;
+          this.selectedClass = "selected";
+          this.trigger = "[data-accordion-element-trigger]";
+          this.content = "[data-accordion-element-content]";
+        }
+
+        setCustomData(data){
+          var keys = Object.keys(data);
+
+          for(var i=0; i<keys.length; i++){
+            this[ keys[i] ] = data[ keys[i] ];
+          }
+        }
+
+        fire(eventName, el){
+          var callbacks = this.events[eventName];
+          for(var i=0; i<callbacks.length; i++){
+            callbacks[i]( el )
+          }
+        }
+
+        on(eventName, cb){
+          if( !this.events[eventName] ) return       
+          this.events[eventName].push(cb);
+        }
+
+        constructor(data){    
+          this.setDefaultData();
+          this.setCustomData(data); // ES6 => Object.assign(this, data)
+
+          [].forEach.call( document.querySelectorAll(this.elements), (el, i)=>{
+            this.els.push({
+              wrapper: el,
+              trigger: el.querySelector(this.trigger),
+              content: el.querySelector(this.content)
+            });
+
+            this.els[i].content.style.height = 0;
+          });
+
+          this.attachEvents();
+        }
+
+      }
+    })();
+
+    var myAccordion = new gianniAccordion({
+      elements: ".card article"
+    });
+
+    myAccordion.on("elementSelected", (data)=>{
+      console.log("elementOpened", data);
+    });
+</script>
 
 <?php 
     get_footer();
